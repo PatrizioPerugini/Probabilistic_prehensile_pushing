925ab6f536fd73ce87b093717521e8fa6fc63d12
PatrizioPerugini <patrizioperugini99@gmail.com>
2024-10-11 14:26:35 +0200

    -h

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Probabilistic_prehensile_pushing
-Run main.py in order to execute the different algorithms, the choice is done by setting either -rrt=1 or -nlp=1
+Run main.py in order to execute the different algorithms, the choice is done by setting either -rrt 1 or -nlp .
+
+To get more details run: python3 main.py -h
--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import argparse
 from in_hand_manipulation.main_rrt import run_RRT
 from MC_OPTIMIZATION.main_nlp import run_NLP
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-rrt', '--RRT', default=1)
-parser.add_argument('-step_s', '--step_size_rrt', default=0.05)
-parser.add_argument('-eps', '--eps_goal_rrt', default=1e-3)
-parser.add_argument('-num_rp', '--num_random_points', default=1)
-parser.add_argument('-nlp', '--NLP', default=0)
-parser.add_argument('-n_steps', '--number_of_steps', default=3)
-parser.add_argument('-p', '--position', default=1)
-parser.add_argument('-t', '--time', default=1)
-parser.add_argument('-le', '--lambda_entropy', default=0.1)
-parser.add_argument('-lp', '--lambda_path', default=0)
-parser.add_argument('-lkl', '--lambda_kl', default=0)
-parser.add_argument('-t_max', '--maximum_time', default=90)
-parser.add_argument('-shape', '--object_shape', default='S' )
+parser = argparse.ArgumentParser(description="Run either the RRT or NLP algorithms for motion planning.")
+
+parser.add_argument('-rrt', '--RRT', default=0, type=int, help = "choose 1 to run RRT")
+parser.add_argument('-step_s', '--step_size_rrt', default=0.05, type=float)
+parser.add_argument('-eps', '--eps_goal_rrt', default=1e-3, type=float)
+parser.add_argument('-num_rp', '--num_random_points', default=1, type=int)
+parser.add_argument('-nlp', '--NLP', default=1, type=int, help = "choose 1 to run NLP")
+parser.add_argument('-n_steps', '--number_of_steps', default=3, type=int, help = "decide how many steps")
+parser.add_argument('-p', '--position', default=0, type=int, help="1 if also position optimized")
+parser.add_argument('-t', '--time', default=0, type=int, help="1 if also time optimized")
+parser.add_argument('-le', '--lambda_entropy', default=0.1, type=float, help="cost for the entropy")
+parser.add_argument('-lp', '--lambda_path', default=0, type=float, help = "cost for path length")
+parser.add_argument('-lkl', '--lambda_kl', default=0, type=float, help = "cost for KL")
+parser.add_argument('-t_max', '--maximum_time', default=90, type=int, help="maximum time allowed for covergence")
+parser.add_argument('-shape', '--object_shape', default='S',help = "choose either S or T" )
 parser.add_argument('-s_all', '--sample_from_all_obj', default=0 )
 #set it to zero for no discretization or decicde the granularity
-parser.add_argument('-d_mc', '--discretization', default=0)
-
-
+parser.add_argument('-d_mc', '--discretization', default=0, type=int,help="set to zero for no discretization or choose the granularity")
 
 args = parser.parse_args()
 
@@ -34,4 +33,6 @@ elif args.RRT == 1:
     run_RRT(args)
 
 else:
+
     raise Exception("Choose one method")
+
